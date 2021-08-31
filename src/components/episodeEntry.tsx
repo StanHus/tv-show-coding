a30cccdad21c7001b74e029dd40a7a2fbb811714
StanHus <guseletovS@gmail.com>
2021-08-31 11:18:53 +0100

    split episodeList, formatted

---
+++ src/components/episodeEntry.tsx
@@ -0,0 +1,69 @@
+interface IEpisode {
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  type: string;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  runtime: number;
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+  _links: { self: { href: string } };
+}
+
+export default function EpisodeEntry(entry: IEpisode): JSX.Element {
+  function SeasonEp(season: number, number: number): string {
+    const output = [];
+    if (season < 10) {
+      output.push(`S0${season}`);
+    } else {
+      output.push(`S${season.toString()}`);
+    }
+
+    if (number < 10) {
+      output.push(`E0${number}`);
+    } else {
+      output.push(`E${number.toString()}`);
+    }
+
+    return output.join("");
+  }
+  function checkText(text: string): string {
+    text = text.replace("<p>", "");
+    text = text.replace("</p>", "");
+    return text;
+  }
+
+  return (
+    <body className="body" key={entry.id}>
+      <section className="elements">
+        <div className="title">
+          <h2>
+            {entry.name} - {SeasonEp(entry.season, entry.number)}
+          </h2>
+        </div>
+        <img className="image" src={entry.image.medium} alt="episode poster" />
+        <div className="part">
+          {/*<p>AirStamp: {entry.airstamp}</p>
+               <p>Airdate: {entry.airdate}</p>
+              <p>AirTime: {entry.airtime}</p>
+              <p>Type: {entry.type}</p>
+              <p>Runtime: {entry.runtime}</p> */}
+          <p>
+            <strong>Summary: </strong>
+            {checkText(entry.summary)}
+          </p>
+          <p>
+            <a href={entry.url}>TV Maze Link</a>
+          </p>
+        </div>
+      </section>
+    </body>
+  );
+}
--- src/components/episodeList.tsx
+++ src/components/episodeList.tsx
@@ -1,77 +1,7 @@
 import episodes from "../episodes.json";
 import "../css/style.css";
 import { useState } from "react";
-
-interface IEpisode {
-  id: number;
-  url: string;
-  name: string;
-  season: number;
-  number: number;
-  type: string;
-  airdate: string;
-  airtime: string;
-  airstamp: string;
-  runtime: number;
-  image: {
-    medium: string;
-    original: string;
-  };
-  summary: string;
-  _links: { self: { href: string } };
-}
-
-function EpisodeEntry(entry: IEpisode): JSX.Element {
-  function SeasonEp(season: number, number: number): string {
-    const output = [];
-    if (season < 10) {
-      output.push(`S0${season}`);
-    } else {
-      output.push(`S${season.toString()}`);
-    }
-
-    if (number < 10) {
-      output.push(`E0${number}`);
-    } else {
-      output.push(`E${number.toString()}`);
-    }
-
-    return output.join("");
-  }
-
-  function checkText(text: string): string {
-    text = text.replace("<p>", "");
-    text = text.replace("</p>", "");
-    return text;
-  }
-
-  return (
-    <body className="body" key={entry.id}>
-      <section className="elements">
-        <div className="title">
-          <h2>
-            {entry.name} - {SeasonEp(entry.season, entry.number)}
-          </h2>
-        </div>
-        <img className="image" src={entry.image.medium} alt="episode poster" />
-        <div className="part">
-          {/*<p>AirStamp: {entry.airstamp}</p>
-           <p>Airdate: {entry.airdate}</p>
-          <p>AirTime: {entry.airtime}</p>
-          <p>Type: {entry.type}</p>
-          <p>Runtime: {entry.runtime}</p> */}
-          <p>
-            <strong>Summary: </strong>
-            {checkText(entry.summary)}
-          </p>
-          <p>
-            <a href={entry.url}>TV Maze Link</a>
-          </p>
-        </div>
-      </section>
-    </body>
-  );
-}
+import EpisodeEntry from "./episodeEntry";
 
 export function EpisodeList(): JSX.Element {
   const [search, setSearch] = useState("");
@@ -96,7 +26,7 @@ export function EpisodeList(): JSX.Element {
             ) {
               ans = val;
             }
-            return ans
+            return ans;
           })
           .map(EpisodeEntry)}
       </div>
